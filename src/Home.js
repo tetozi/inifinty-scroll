166eaf7794f95d5f3411b3d4d3c76ecdf2d0823f
Alkesandar Yanchev <48323569+tetozi@users.noreply.github.com>
2022-07-06 15:15:03 +0300

    Add files via upload

---
+++ src/Home.js
@@ -0,0 +1,69 @@
+import Product from "./product/Product"
+import { useEffect, useState, useRef } from "react"
+
+import * as Service from './Service'
+import Loading from './loading.gif'
+
+const Home = () => {
+
+    const [product, setProduct] = useState([])
+    const [pageNumber, setPageNumber] = useState(0)
+    const [loading, setLoading] = useState(false)
+    const [error, setError] = useState('')
+
+
+    useEffect(() => {
+        Service.getAll(pageNumber)
+            .then(result => {
+                if (result == "Something went wrong. Please retry!") {
+                    setError(result)
+                    console.log(error)
+
+                } else {
+                    setProduct(result);
+                    setLoading(true)
+                }
+            })
+
+    }, [pageNumber]);
+
+    const loadMore = () => {
+        setPageNumber(prevPageNumber => prevPageNumber + 1)
+    }
+
+
+    const pageEnd = useRef()
+
+    useEffect(() => {
+        if (loading) {
+            const observer = new IntersectionObserver(entries => {
+                if (entries[0].isIntersecting) {
+                    loadMore()
+                }
+            },
+                { threshold: 1 });
+            observer.observe(pageEnd.current)
+        }
+    }, [loading])
+    return (
+        <>
+            {
+                product.length > 0
+                    ? (
+                        <ul className="overview">
+                            {product.map(x => <Product key={x.name} product={x} />)}
+                        </ul>
+                    )
+                    : <p>No course in database!</p>
+            }
+            <div className="loading">
+                <img src={Loading} alt="loading" />
+            </div>
+            <p ref={pageEnd}>end</p>
+        </>
+
+    )
+
+}
+
+export default Home
